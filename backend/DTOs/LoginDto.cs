9f15e3f24e0bb88758e306f66d37cb6e4b2e44ee
byrontorres26 <byrontorres@unitec.edu>
2026-06-17 18:45:20 -0600

    Eliminacion de las carpetas vacias que cree por error

--- backend/DTOs/LoginDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Proyecto_PrograWeb_Team1.DTOs;
-
-public class LoginDto
-
-{
-    // Lo que el frontend manda cuando alguien quiere entrar
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
